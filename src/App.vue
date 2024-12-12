66b72e15b72e6062ff2350dcf09a949723f9897d
zhoucheng <zhoucheng133@outlook.com>
2024-12-12 20:11:21 +0800

    添加基本信息显示

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-    Hello World!
+  <div class="content">
+    <div class="hello">Welcome back!</div>
+    <div class="subtitle">Monitor Panel</div>
+    <baseinfo/>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { initData } from './hooks/requests';
+import baseinfo from './components/baseinfo.vue';
 
 
 initData();
 
 </script>
+
+<style>
+body{
+  display: flex;
+  justify-content: center;
+  background-color: rgb(250, 250, 250);
+}
+</style>
+
+<style scoped>
+.hello{
+  font-size: 30px;
+}
+.content{
+  width: 1200px;
+  background-color: white;
+  box-sizing: border-box;
+  padding: 20px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  font-family: Lucida Console;
+}
+@media screen and (max-width: 1200px) {
+  .content {
+    width: 100vw;
+  }
+}
+</style>
---
+++ src/components/baseinfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="baseInfo">
+    <div class="infoContainer" style="background-color: black; color: white;">
+      <div class="infofont">System</div>
+      <div class="valuefont">{{ data.systemData.family }}</div>
+    </div>
+    <div class="infoContainer">
+      <div class="infofont">Version</div>
+      <div class="valuefont">{{ data.systemData.version }}</div>
+    </div>
+    <div class="infoContainer">
+      <div class="infofont">Manufacturer</div>
+      <div class="valuefont">{{ data.systemData.manufacturer }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import store from '../hooks/store';
+
+let { data }=storeToRefs(store());
+
+
+</script>
+
+<style scoped>
+.valuefont{
+  margin-top: 10px;
+  font-weight: bold;
+  font-size: 30px;
+}
+.infoContainer{
+  border-radius: 10px;
+  height: 150px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+  padding: 20px;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.baseInfo{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+  margin-top: 10px;
+}
+</style>
